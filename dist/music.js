1cb21fdcbf67c54c378522b9bc8a5caf9b9e5bfb
CallMeTe <CallMema@protonmail.com>
2022-05-27 02:55:50 +0800

    Site updated: 2022-05-27 02:55:50

--- dist/music.js
+++ dist/music.js
@@ -1,19 +1,13 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
 	{
         name: "平凡之路",
         artist: '----朴树',
-        url: 'http://www.ytmp3.cn/down/59211.mp3',
+        url: 'http://music.163.com/song/media/outer/url?id=500665346.mp3',
         cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
-      },
-	  {
-        name: '这些民谣一次听个够',
-        artist: '----whois',
-        url: 'http://www.ytmp3.cn/down/60222.mp3',
-        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
       },
 	 {
         name: '像鱼',
